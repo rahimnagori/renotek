33609e9f9793e6d4fdc6b5595867fb0b11d66425
rahim <rahim.nagori@gmail.com>
2022-12-19 22:31:31 +0530

    UI formatting

--- application/views/site/about.php
+++ application/views/site/about.php
@@ -29,84 +29,24 @@
     <div class="conten_set ">
       <h4>Why Renotek LED Products</h4>
       <p>We are highly committed our product quality and performance, all our products speak of high quality and standards. Not only products meet most of the established Industry norms, but our manufacturing processes, component sourcing and In-house R&D can be benchmarked to the best in the Industry. We are capable of meeting prescribed operating norms and guidelines of most of the Countries. Our critical key components are sourced only from us and German companies.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Long Life</h4>
       <p>Renotek products Invariably carry a long life time and this stands out as the number one benefit of our LED products, Renotek LED bulbs and diodes have on outstanding operotlonal life time expectation of up to 1.00.000 hours. this Is 11 years of continuous operation. Or 22 years of 50% operation. If you leave a Renotek switched on for 8 hours per day It would take around 20 years before you'd have to replace a Renotek LED bulb.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Energy Efficiency</h4>
       <p>Renotek LED lamps today is the most efficient way of illumination and lighting, with an estimated energy efficiency of 80%•90% when compared to traditional lighting and conventional light bulbs. This means that about 80% of the electrical energy Is convened to light.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Ecologically Frtendly</h4>
       <p>Renotek LED products are rlee of toxic chemicals-al lour products are environment friendly. Compared to Renotek LED, most conventional fluorescent lighting bulbs contain a multitude of materials like e.g. mercury that ore dangerous for the environment. Renotek LED products contain no toxic materials and are 100% recyclable, and will help you to reduce your carbon footprint by up to a third. The long operational lifetime span mentioned above means also that one Renotek LED light bulb can save material and production of 25 incandescent light bulbs. A big step towards a greener future.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Durable Quality</h4>
       <p>Renotek LEDs are extremely durable and built with sturdy components that are highly rugged and can withstand even the roughest conditions. Because Renotek LED products are resistant to shock, vibrations and external Impacts, they make great outdoor lighting systems for rough conditions and exposure to weather. Wind, rain or even external vandalism, traffic related public exposure and construction or manufacturing sites.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Zero UV Emissions</h4>
       <p>Renotek LED Illumination produces little Infrared Light and close to no UV emissions. Because of this, our LED lighting solution Is highly suitable nol only for goods and materials that are sensitive to heat due to the benefit of little radiated heat emission, but also for Illumination of UV sensitive objects or materials such as In museums, art galleries, archeological sites etc.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Design Flexibility</h4>
       <p>Renotek products have significant acceptability with architectures and interior designers- In fact our products are the first choice for Interior designers In most developed economies. Renotek portfolio of LED lights can be combined in any shape to produce highly efficient illumination. Individual Renotek LEDs can be dimmed. Resulting in a dynamic control of light, color and distribution. With Renotek we can have well-designed LED Illumination systems which can achieve beautiful and pleasant lighting effects, not only for eye but also for the mood and the mind. Renotek has also developed smart solutions In this area which work on modern day Internet of things(lot) platform and capable of Integrating Itself with Amazon Alexa, Google and Apple Home (Amazon Alexa, Google and apple are brands own by respective owner Renotek dose not own these brand ) increasingly, smart integrated lot based Renotek LED solutions are finding applications In smart offices and smart homes.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Operational in Extremely Cold or Hot Temperatures</h4>
       <p>Renotek LED solutions are Ideal for operation under cold and low outdoor temperature settings. For fluorescent lamps, low temperatures may affect operation and present a challenge, but Renotek LED illumination operates well also In cold settings, such as for outdoor winter settings, freezer rooms etc.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Light Dispersion</h4>
       <p>Renotek LED solutions are designed to focus its light and can be directed to a specific location without the use of an external reflector, achieving a higher application efficiency than conventional lighting. Our well-designed Renotek LED illumination systems are able to deliver light more efficiently to the desired location.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Instant lighting &Frequent Switching</h4>
       <p>Renotek LED lights brighten up immediately and when powered on, has great advantages for Infrastructure project such as traffic and signal lights. Also, Renotek LED lights can be switched off and on frequently without affecting the product's life or light emission. In contrast, traditional lighting may take several seconds to reach full brightness and frequent On/Off switching does drastically reduce operational life expectancy.</p>
-    </div>
-  </div>
-</div>
-<div class="sec_pad">
-  <div class="container">
-    <div class="conten_set ">
       <h4>Low-Voltage</h4>
       <p>A Low-voltage power, supply is sufficient for Renotek LED illumination. This makes It easy to use Renotek LED lighting also in outdoor settings, by connecting on external solor-energy source. This Is a big advantage when It comes to using Renotek LED technology In remote or rural areas.</p>
       <p>Did these top 10 benefits of Renotek LED lighting convinced you ? Renotek LED lighting has even more benefits and advantages, makesure you use Renotek LED solutions and save both the planet-and your money!</p>
